39e9c0b5a95e3b926838132b3e3a61210ff5527d
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-09-01 12:42:04 +0300

    Add boolean field is_response
    is_response will show that the comment is a response to another one
    even if comment_to_response field is set to Null due to the responsed comment
    was deleted

--- tasker/app/models/comment.py
+++ tasker/app/models/comment.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from . import Base
 
 
@@ -10,14 +10,19 @@ class Comment(Base):
     date = Column(DateTime, nullable=False, default=datetime.datetime.now)
     body = Column(String(255), nullable=False)
     comment_to_response = Column(Integer, ForeignKey('comments.id'), nullable=True)
+    is_response = Column(Boolean, nullable=False, default=False)
 
     def __init__(self, date, body, comment_to_response=None):
         self.date = date
         self.body = body
         self.comment_to_response = comment_to_response
+        if comment_to_response:
+            self.is_response = True
+        else:
+            self.is_response = False
 
     def __repr__(self):
-        if self.comment_to_response:
+        if self.is_response:
             return f"Comment #{self.id}:\n\tDate:{self.date}\n\t" \
                    f"Answer to {self.comment_to_response} Body:{self.body}"
         else:
