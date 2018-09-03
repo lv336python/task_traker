ba3ca5117f22b6085d2f61b45bf07f8efcc3c150
underdog18 <oleksandr.proshchuk@gmail.com>
2018-09-04 01:17:13 +0300

    modified user model user.py

--- tasker/app/models/user.py
+++ tasker/app/models/user.py
@@ -9,15 +9,17 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
-
-    def set_password(self, pw):
-        self.password = generate_password_hash(pw)
+    @staticmethod
+    def create (username, password):
+        user = User()
+        user.username = username
+        user.password = generate_password_hash(password)
+        db.session.add(user)
+        db.session.commit()
 
     def check_password(self, pw):
         return check_password_hash(self.password, pw)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
