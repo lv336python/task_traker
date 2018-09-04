ba3fffe89a49e1d53d163c8dfa8499e986cde70f
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-09-04 16:22:24 +0300

    Change comment model and view

--- tasker/app/routers/comment.py
+++ tasker/app/routers/comment.py
@@ -1,8 +1,8 @@
 import json
 import datetime
-from flask import request
+from flask import request, session, abort
 
-from app.models import Comment
+from app.models import Comment, User
 from .. import app, db
 
 
@@ -15,6 +15,7 @@ def comments():
     objects and creates a new comment
     :return: String with verbal status of response
     """
+    session['user_id'] = 1
     if request.method == "GET":
         response = []
         for comment in Comment.query.all():
@@ -22,19 +23,27 @@ def comments():
                              'date': comment.date.strftime("%Y:%M:%D  %H:%M:%S"),
                              'body': comment.body,
                              'comment_to_response': comment.comment_to_response,
-                             'is_response': comment.is_response})
-        return json.dumps(response)
+                             'is_response': comment.is_response,
+                             'user_id': comment.user_id})
+        return json.dumps(response), 200
 
     else:
         body = request.form["body"]
         comment_to_response = request.form.get("comment_to_response", None)
+        user_id = session.get('user_id', None)
+        if user_id is None:
+            abort(401)
         if comment_to_response and not Comment.query.filter(Comment.id == comment_to_response).first():
-            return "FAILED"
+            return
         else:
-            new_comment = Comment(datetime.now(), body=body, comment_to_response=comment_to_response)
-            db.add(new_comment)
-            db.commit()
-            return "ADDED\n" + str(new_comment)
+
+            new_comment = Comment(datetime.datetime.now(),
+                                  body=body,
+                                  comment_to_response=comment_to_response,
+                                  user_id=user_id)
+            db.session.add(new_comment)
+            db.session.commit()
+            return new_comment.to_json(), 201
 
 
 
@@ -45,19 +54,21 @@ def comment(comment_id):
     :param comment_id: integer which represents comment id in DB
     :return: String with verbal status of response
     """
-    if Comment.query.filter(Comment.id == comment_id).first():
-        if request.method == "PUT":
-            body = request.form.get("body", None)
-            if body:
-                comment_to_update = Comment.query.filter(Comment.id == comment_id).first()
-                comment_to_update.body = body
-                db.commit()
-                return "UPDATED\n" + str(comment_to_update)
+    comment_to_update = Comment.query.filter(Comment.id == comment_id).first()
+    if comment_to_update:
+        if session['user_id'] == comment_to_update.user_id:
+            if request.method == "PUT":
+                body = request.form.get("body", None)
+                if body:
+                    comment_to_update.body = body
+                    db.commit()
+                    return "UPDATED\n" + str(comment_to_update)
+                else:
+                    return "FAILED"
+
             else:
-                return "FAILED"
-        else:
-            db.delete(Comment.query.filter(Comment.id == comment_id).first())
-            db.commit()
-            return "DELETED"
+                db.delete(Comment.query.filter(Comment.id == comment_id).first())
+                db.commit()
+                return "DELETED"
     else:
         return "FAILED"
