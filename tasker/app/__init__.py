1147bd99cc8630e5e70668b60eef32ee1ba487bc
AnyaShymanska <ani4ka30@gmail.com>
2018-09-04 15:02:27 +0300

    change app init

--- tasker/app/__init__.py
+++ tasker/app/__init__.py
@@ -11,6 +11,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+app.secret_key = 'any random string'
 
 
 from .routers import (profile,
